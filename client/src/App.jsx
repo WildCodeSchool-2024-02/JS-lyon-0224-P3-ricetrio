023a23cc27cc060fc8bf143e093f48f392fe15f5
Thien Nguyen <thien.ngu25@gmail.com>
2024-06-06 11:04:36 +0200

    delete figma url and add component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Inscription from "./pages/SignUp/Inscription";
 import Signin from "./pages/SignIn/Signin";
 
 function App() {
   return (
     <main className="container">
+      <Inscription />
       <Signin />
     </main>
   );
