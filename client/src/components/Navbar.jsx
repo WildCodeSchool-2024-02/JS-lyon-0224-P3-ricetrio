37a0f6b1693200314abf7d956c9023ddf953b8db
Thien Nguyen <thien.ngu25@gmail.com>
2024-06-07 10:09:12 +0200

    fix majuscule des images

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,22 @@
-import logo from "../assets/images/Kat.png";
-import search from "../assets/images/Search.png";
-import signin from "../assets/images/Sign-in.png";
+import Logo from "../assets/images/Kat.png";
+import Search from "../assets/images/Search.png";
+import Signin from "../assets/images/Sign-in.png";
 import styles from "./Navbar.module.css";
 
 function NavBar() {
   return (
     <div className={styles.containParents}>
       <div className={styles.containLeft}>
-        <img src={logo} alt="logo KAT" className={styles.logo} />
+        <img src={Logo} alt="logo KAT" className={styles.logo} />
       </div>
       <div className={styles.containRight}>
         <img
-          src={search}
+          src={Search}
           alt="search with the magnifying glass"
           className={styles.search}
         />
         <img
-          src={signin}
+          src={Signin}
           alt="profil for signin or signup"
           className={styles.profile}
         />
