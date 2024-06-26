a173ee271c9271e4be3e830d0159961279d5b1a6
Thien Nguyen <thien.ngu25@gmail.com>
2024-06-26 23:44:41 +0200

    add joi validation on user

--- client/src/pages/SignUp/InscriptionValidation.js
+++ client/src/pages/SignUp/InscriptionValidation.js
@@ -2,7 +2,7 @@ function Validation(values) {
   const errors = {};
   const pseudoPattern = /^[a-zA-Z][a-zA-Z0-9]{2,15}$/;
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,}$/;
 
   if (values.pseudo.trim() === "") {
     errors.pseudo = "Le pseudo est requis";
@@ -20,9 +20,9 @@ function Validation(values) {
     errors.password = "Le mot de passe est requis";
   } else if (!passwordPattern.test(values.password)) {
     errors.password =
-      "Le mot de passe doit contenir au moins 8 caractères, une lettre majuscule, une lettre minuscule et un chiffre";
-  } else if (values.password.trim().length > 20) {
-    errors.password = "Le mot de passe ne doit pas dépasser 20 caractères";
+      "Le mot de passe doit contenir au moins 12 caractères, une lettre majuscule, une lettre minuscule et un chiffre";
+  } else if (values.password.trim().length > 255) {
+    errors.password = "Le mot de passe ne doit pas dépasser 255 caractères";
   }
 
   return errors;
