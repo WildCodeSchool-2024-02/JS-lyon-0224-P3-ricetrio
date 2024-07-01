bbcde7366811216f0309e5956945c91f9d7d5e59
Thien Nguyen <thien.ngu25@gmail.com>
2024-07-01 16:50:18 +0200

    sauvetage du maincarousel suite à reset hard

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,12 @@
 import NavBar from "../../components/Navbar/Navbar";
-import MainCarousel from "../../components/Home/MainCarousel";
+import CarouselWrapper from "../../components/Home/CarouselWrapper";
 import FilterSection from "../../components/Home/FilterSection";
 
 function Homepage() {
   return (
     <div>
       <NavBar />
-      <MainCarousel />
+      <CarouselWrapper />
       <FilterSection />
     </div>
   );
