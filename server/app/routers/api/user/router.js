087ecaa26cee30f8d016781b6c9748f4aa6a967c
Thien Nguyen <thien.ngu25@gmail.com>
2024-07-09 16:01:13 +0200

    fix admin role

--- server/app/routers/api/user/router.js
+++ server/app/routers/api/user/router.js
@@ -10,12 +10,11 @@ const router = express.Router();
 const { browse, add } = require("../../../controllers/userActions");
 const { hashPassword } = require("../../../services/auth");
 const validateSignIn = require("../../../services/validateSignIn");
-const checkAdminRole = require("../../../services/checkAdminRole");
 
 // Route to get a list of offers
 router.get("/", browse);
 
-router.post("/", hashPassword, validateSignIn, checkAdminRole, add);
+router.post("/", hashPassword, validateSignIn, add);
 
 /* ************************************************************************* */
 
