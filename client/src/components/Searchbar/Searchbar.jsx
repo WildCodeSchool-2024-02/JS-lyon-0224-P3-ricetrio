a5d58c76bc9f884d6cf0961ffa8a2bf51c15793a
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-11 23:31:23 +0200

    change list to image

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import { useLoaderData, Link } from "react-router-dom";
 import styles from "./searchbar.module.css";
 
 function Searchbar() {
+  const allFilms = useLoaderData();
+  const [searchFilm, setSearchFilm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchResult = (value) =>
+    allFilms.filter(
+      (film) =>
+        value &&
+        film &&
+        film.title &&
+        film.title.toLowerCase().includes(value.toLowerCase())
+    );
+
+  const handleSearchFilm = (value) => {
+    setSearchFilm(value);
+    setResults(searchResult(value));
+  };
+
   return (
     <div className={styles.searchbarContainer}>
-      <p>kana</p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          className={styles.searchbarInput}
+          type="text"
+          placeholder="Cherchez votre film"
+          name="film"
+          value={searchFilm}
+          onChange={(e) => handleSearchFilm(e.target.value)}
+        />
+      </form>
+      <button className={styles.searchButton} type="submit">
+        Recherche
+      </button>
+      <ul>
+        {results.map((film) => (
+          <Link key={film.id} to={`/bandeannonce/${film.id}/`}>
+        
+            <li key={film.id}>{film.title}</li>
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 }
