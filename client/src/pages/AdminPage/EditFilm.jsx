8d1b0f2adf2a94bc6ebc3ab29f17c857daa01139
Thien Nguyen <thien.ngu25@gmail.com>
2024-07-11 15:02:12 +0200

    module

--- client/src/pages/AdminPage/EditFilm.jsx
+++ client/src/pages/AdminPage/EditFilm.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import styles from "./editFilm.module.css";
+import styles from "./edit.module.css";
 
 function EditFilm() {
   const api = import.meta.env.VITE_API_URL;
