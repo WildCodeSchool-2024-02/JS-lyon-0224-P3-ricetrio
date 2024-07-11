09c3019beb6b2ade58d4e5eb79c35b4c0fe095ba
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-11 14:58:34 +0200

    change branch

---
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./searchbar.module.css";
+
+function Searchbar() {
+  return (
+    <div className={styles.searchbarContainer}>
+      <p>kana</p>
+    </div>
+  );
+}
+
+export default Searchbar;
