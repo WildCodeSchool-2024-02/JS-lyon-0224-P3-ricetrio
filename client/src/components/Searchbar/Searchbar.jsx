11cf8f0ee6d5c3fdf515dc9fb3dada76bb40958a
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-12 09:46:44 +0200

    change branch

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useLoaderData, Link } from "react-router-dom";
+import { useUserContext } from "../../contexts/UserContext";
 import styles from "./searchbar.module.css";
 
 function Searchbar() {
   const allFilms = useLoaderData();
   const [searchFilm, setSearchFilm] = useState("");
   const [results, setResults] = useState([]);
+  const { user } = useUserContext();
 
   const searchResult = (value) =>
     allFilms.filter(
@@ -23,7 +25,7 @@ function Searchbar() {
 
   return (
     <div className={styles.searchbarContainer}>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => e.preventDefault()} disabled={!user}>
         <input
           className={styles.searchbarInput}
           type="text"
@@ -39,7 +41,6 @@ function Searchbar() {
       <ul>
         {results.map((film) => (
           <Link key={film.id} to={`/bandeannonce/${film.id}/`}>
-        
             <li key={film.id}>{film.title}</li>
           </Link>
         ))}
